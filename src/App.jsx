83a6cb758359927c0eb8aa3cb0a7282992b0bece
Smogg21 <spazzaglia98@gmail.com>
2024-03-30 14:03:58 -0500

    Created custom hook useMovies

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import "./App.css";
 import { Movies } from "./components/Movies";
-import responseMovies from "./mocks/with-results.json";
-// import witouthResults from "./mocks/no-results.json";
+import { useMovies } from "./hooks/useMovies";
 
 //https://www.omdbapi.com/?apikey=7c5962cc&s=Avengers
 //API_KEY = 7c5962cc
 
+
 function App() {
-  const movies = responseMovies.Search;
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
+  const {movies: mappedMovies} = useMovies()
+
   return (
     <div className="page">
       <header>
