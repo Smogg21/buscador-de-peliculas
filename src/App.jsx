cefa7d3c287024ad935462cbbc0cf78542471372
Smogg21 <spazzaglia98@gmail.com>
2024-03-30 12:49:32 -0500

    Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+// import witouthResults from "./mocks/no-results.json";
 
 //https://www.omdbapi.com/?apikey=7c5962cc&s=Avengers
 //API_KEY = 7c5962cc
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <div className="page">
       <header>
@@ -18,7 +22,21 @@ function App() {
           <button type="submit">Buscar</button>
         </form>
       </header>
-      <main>Aquí irán los resultados</main>
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se encontraron películas para esta búsqueda</p>
+        )}
+      </main>
     </div>
   );
 }
