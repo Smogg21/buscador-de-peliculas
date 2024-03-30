174aa7cd2c7bc55f7134b1b93b0d3101d159bc83
Smogg21 <spazzaglia98@gmail.com>
2024-03-30 11:55:31 -0500

    Created user Mock DATA

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+//https://www.omdbapi.com/?apikey=7c5962cc&s=Avengers
+//API_KEY = 7c5962cc
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page">
+      <header>
+        <h1>Buscador de películas</h1>
+        <form action="" className="form">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Avengers, Star Wars, The matrix ..."
+          />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>Aquí irán los resultados</main>
+    </div>
+  );
 }
 
-export default App
+export default App;
