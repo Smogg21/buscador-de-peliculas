c3f8240b789ddac610225ba33d37a47cc4e1437a
Smogg21 <spazzaglia98@gmail.com>
2024-04-01 12:46:09 -0500

    Added useRef from dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
