7bb368efbd0c5fda89f28f4474a2f4bf30640e15
Smogg21 <spazzaglia98@gmail.com>
2024-04-01 22:18:08 -0500

    Modified fetch to a service. Transformed it to an async function

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,29 +1,14 @@
-// import withResults from "../mocks/with-results.json";
-import witouthResults from "../mocks/no-results.json";
 import { useState } from "react";
+import { searchMovies } from "../services/movies";
 
 export function useMovies ({search}) {
-    const [responseMovies, setResponseMovies] = useState([]);
-    const movies = responseMovies.Search;
-    const mappedMovies = movies?.map((movie) => ({
-      id: movie.imdbID,
-      title: movie.Title,
-      year: movie.Year,
-      poster: movie.Poster,
-    }));
+    const [movies, setMovies] = useState([]);
 
-    const getMovies = () =>{
-      if (search){
-        // setResponseMovies(withResults)
-        fetch(`https://www.omdbapi.com/?apikey=7c5962cc&s=${search}`)
-        .then (res => res.json())
-        .then (json => setResponseMovies(json))
-      }
-      else{
-        setResponseMovies(witouthResults)
-      }
+    const getMovies = async () =>{
+      const newMovies = await searchMovies({search})
+      setMovies(newMovies)
     }
   
-    return {movies: mappedMovies, getMovies}
+    return {movies, getMovies}
   }
   
