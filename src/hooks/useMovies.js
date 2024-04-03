ca664ace4b4c47cdbfb0099189adaee9c8e3f743
Smogg21 <spazzaglia98@gmail.com>
2024-04-02 23:08:30 -0500

    Small bugfixes

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -4,6 +4,7 @@ import { searchMovies } from "../services/movies";
 export function useMovies({ search, sort }) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
