0d08c4041d943469d06c1ff72e7cdfd8a1be79ed
Smogg21 <spazzaglia98@gmail.com>
2024-04-02 22:23:31 -0500

    Added useMemo to getMovies function (increase performance)

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,34 +1,34 @@
-import { useState, useRef, useMemo} from "react";
+import { useState, useRef, useMemo } from "react";
 import { searchMovies } from "../services/movies";
 
-export function useMovies ({search, sort}) {
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const previousSearch = useRef(search)
+export function useMovies({ search, sort }) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const previousSearch = useRef(search);
 
-    const getMovies = async () =>{
-      if(search === previousSearch.current) return
-      try{
-        setLoading(true)
-        setError(null)
-        previousSearch.current = search
-        const newMovies = await searchMovies({search})
-        setMovies(newMovies)
-      } catch(e) {
-        setError(e.message)
+  const getMovies = useMemo(() => {
+    return async () => {
+      if (search === previousSearch.current) return;
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (e) {
+        setError(e.message);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
-    
-    const sortedMovies = useMemo(() => {
-      return sort  
-      ? [...movies].sort((a,b) => a.title.localeCompare(b.title))
-      : movies
-    }, [sort, movies])
+    };
+  }, [search]);
 
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies;
+  }, [sort, movies]);
 
-    return {movies: sortedMovies, getMovies, loading}
-  }
-  
+  return { movies: sortedMovies, getMovies, loading };
+}
